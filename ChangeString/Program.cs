c1c707906f8ff50105ed9f72686a232949f872b2
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-06 10:50:41 +0000

    ChangeString finished

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,27 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            
+            string str, chr;
+            char convertChar;
+            Console.Write("Insert a string: ");
+            str = Console.ReadLine();
+            Console.Write("Insert a character: ");
+            chr = Console.ReadLine();
+
+            convertChar = char.Parse(chr);
+
+            foreach (char chr2 in str)
+            {
+                if (chr2 == convertChar)
+                {
+                    convertChar = 'x';
+                    Console.Write($"{convertChar}");
+                }
+                else
+                {
+                    Console.Write($"{chr2}");
+                }
+            }
         }
     }
 }
