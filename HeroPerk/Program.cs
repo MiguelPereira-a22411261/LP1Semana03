91cf06b3811480298da87a5231816e78d539010d
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-06 21:50:04 +0000

    Read input character by character

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -2,11 +2,47 @@
 
 namespace HeroPerk
 {
+    [Flags]
+    enum Perks
+        {
+            WarpShift = 1 << 0 ,
+            Stealth = 1 << 0,
+            AutoHeal = 1 << 0,
+            DoubleJump = 1 << 0
+        }
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Perks perks = 0;
+            Perks silJ = Perks.Stealth | Perks.DoubleJump;
+            bool unkPerk = false;
+            string input = args[0];
+
+            foreach (char chr in input)
+            {
+                if (chr == 'w')
+                {
+                    perks ^= Perks.WarpShift;
+                }
+                else if (chr == 'a')
+                {
+                    perks ^= Perks.AutoHeal;
+                }
+                else if (chr == 's')
+                {
+                    perks ^= Perks.Stealth;
+                }
+                else if (chr == 'd')
+                {
+                    perks ^= Perks.DoubleJump;
+                }
+                else
+                {
+                    unkPerk = true;
+                    break;
+                }
+            }
         }
     }
 }
