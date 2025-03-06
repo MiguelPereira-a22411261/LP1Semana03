8dc99f0cf134ce175ee3f6b5392da760b6a211f6
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-06 21:10:28 +0000

    Finished ArrayMul

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection;
 
 namespace ArrayMul
 {
@@ -6,13 +8,15 @@ namespace ArrayMul
     {
         private static void Main(string[] args)
         {
+           CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+           
            float [,] matrix = new float[2,2];
            matrix [0,0] = float.Parse(args[0]);
            matrix [0,1] = float.Parse(args[1]);
            matrix [1,0] = float.Parse(args[2]);
            matrix [1,1] = float.Parse(args[3]);
 
-           float [] matrix2 = new float[2];
+           float[] matrix2 = new float[2];
            matrix2 [0] = float.Parse(args[4]);
            matrix2 [1] = float.Parse(args[5]);
 
@@ -20,13 +24,15 @@ namespace ArrayMul
 
            for (int val1 = 0; val1 < matrix.GetLength(0); val1++)
            {
-                float resultMult = 0;
+                endMatrix[val1] = 0;
                 for (int val2 = 0; val2 < matrix.GetLength(0); val2++)
                 {
-                    resultMult += matrix[val1, val2] * matrix2[val2];
+                    endMatrix[val1] += matrix[val1, val2] * matrix2[val2];
                 }
-                endMatrix[val1] = resultMult;
-                Console.WriteLine(endMatrix[val1]);
+           }
+           foreach (float value in endMatrix)
+           {
+                Console.WriteLine($"| {value, 7:f2} |");
            }
         }
     }
